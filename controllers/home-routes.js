e023197c8c39fdafe8422637cf36b2ff283613f3
shd327 <115894243+shd327@users.noreply.github.com>
2022-12-12 10:13:13 -0700

    Shd327 fix login logout branch issue (#12)
    * init package
    
    * added try block in home route signup
    
    * fixed homeroutes syntax issues:
    
    * remove extra login get route
    
    Co-authored-by: Hiro Protagonist <shd327@gmail.com>

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,53 +1,52 @@
 const router = require("express").Router();
 const { User, List } = require("../models");
+const { withAuth } = require("../utils/auth")
 
-router.get("/", async (req, res) => {
-  // res.status(200).json(userData)
 
 
 router.get('/', async (req, res) => {
 
-    // res.status(200).json(userData)
+  // res.status(200).json(userData)
 
-    // res.status(200).json(userData)
-    res.render('login'
+  // res.status(200).json(userData)
+  res.render('login'
 
-        // loggedIn: req.session.loggedIn,
-    );
+    // loggedIn: req.session.loggedIn,
+  );
 
 
 
 });
 
 // Login route
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-    res.render('login');
-});
+// router.get('/login', withAuth, (req, res) => {
+//   // if (req.session.loggedIn) {
+//   res.redirect('/');
+//   // return;
+//   // }
+//   res.render('login');
+// });
 
 
 router.get('/homepage', async (req, res) => {
-    try {
+  try {
 
-        const userData = await User.findAll({
-            include: [{ model: List }]
-        })
-        // res.status(200).json(userData)
-        const users = userData.map((user) =>
-            user.get({ plain: true })
-        );
-        // res.status(200).json(userData)
-        res.render('homepage', {
-            users
-            // loggedIn: req.session.loggedIn,
-        });
-    }
-    catch (err) {
-        res.status(400).json(err)
-    }
+    const userData = await User.findAll({
+      include: [{ model: List }]
+    })
+    // res.status(200).json(userData)
+    const users = userData.map((user) =>
+      user.get({ plain: true })
+    );
+    // res.status(200).json(userData)
+    res.render('homepage', {
+      users
+      // loggedIn: req.session.loggedIn,
+    });
+  }
+  catch (err) {
+    res.status(400).json(err)
+  }
 
   // res.status(200).json(userData)
   res.render(
@@ -66,39 +65,40 @@ router.get("/signup", async (req, res) => {
     "signup"
 
 
-        )
-
-        const user = userData.get({ plain: true });
-        res.status(200).json(userData)
-        res.render('homepage', {
-            user
-            // loggedIn: req.session.loggedIn,
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-
-    // loggedIn: req.session.loggedIn,
-  );
-});
-
-
-router.get("/homepage", async (req, res) => {
+  )
   try {
-    const userData = await User.findAll({
-      include: [{ model: List }],
-    });
-    // res.status(200).json(userData)
-    const users = userData.map((user) => user.get({ plain: true }));
-    // res.status(200).json(userData)
-    res.render("homepage", {
-      users,
+    const user = userData.get({ plain: true });
+    res.status(200).json(userData)
+    res.render('homepage', {
+      user
       // loggedIn: req.session.loggedIn,
     });
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
-});
+
+  // loggedIn: req.session.loggedIn,
+})
+
+
+
+
+// router.get("/homepage", async (req, res) => {
+//   try {
+//     const userData = await User.findAll({
+//       include: [{ model: List }],
+//     });
+//     // res.status(200).json(userData)
+//     const users = userData.map((user) => user.get({ plain: true }));
+//     // res.status(200).json(userData)
+//     res.render("homepage", {
+//       users,
+//       // loggedIn: req.session.loggedIn,
+//     });
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 router.get("/:id", async (req, res) => {
   try {
