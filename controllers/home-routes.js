1236134a82502cc07728bb8e579087a6228f64de
Hiro Protagonist <shd327@gmail.com>
2022-12-12 09:52:15 -0700

    remove extra login get route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
 const { User, List } = require("../models");
+const { withAuth } = require("../utils/auth")
 
-router.get("/", async (req, res) => {
-  // res.status(200).json(userData)
-  res.send('hello world')
-})
+
 
 router.get('/', async (req, res) => {
 
@@ -21,13 +19,13 @@ router.get('/', async (req, res) => {
 });
 
 // Login route
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-  res.render('login');
-});
+// router.get('/login', withAuth, (req, res) => {
+//   // if (req.session.loggedIn) {
+//   res.redirect('/');
+//   // return;
+//   // }
+//   res.render('login');
+// });
 
 
 router.get('/homepage', async (req, res) => {
@@ -85,22 +83,22 @@ router.get("/signup", async (req, res) => {
 
 
 
-router.get("/homepage", async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      include: [{ model: List }],
-    });
-    // res.status(200).json(userData)
-    const users = userData.map((user) => user.get({ plain: true }));
-    // res.status(200).json(userData)
-    res.render("homepage", {
-      users,
-      // loggedIn: req.session.loggedIn,
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+// router.get("/homepage", async (req, res) => {
+//   try {
+//     const userData = await User.findAll({
+//       include: [{ model: List }],
+//     });
+//     // res.status(200).json(userData)
+//     const users = userData.map((user) => user.get({ plain: true }));
+//     // res.status(200).json(userData)
+//     res.render("homepage", {
+//       users,
+//       // loggedIn: req.session.loggedIn,
+//     });
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 router.get("/:id", async (req, res) => {
   try {
