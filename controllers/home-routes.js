d64db39064064d498c1af875495de9389ee1a88e
Hiro Protagonist <shd327@gmail.com>
2022-12-14 15:49:11 -0700

    got update and rendering of friends to show on friends page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,29 +3,19 @@ const { User, List } = require("../models");
 const { withAuth } = require("../utils/auth");
 
 router.get("/", async (req, res) => {
-  // res.status(200).json(userData)
-
-  // res.status(200).json(userData)
   res.render(
     "login"
-
-    // loggedIn: req.session.loggedIn,
   );
 });
 
 router.get("/signup", async (req, res) => {
-  // res.status(200).json(userData)
-
-  // res.status(200).json(userData)
   res.render(
     "signup"
-
-    // loggedIn: req.session.loggedIn,
   );
 });
 
 router.get("/friends", async (req, res) => {
-  // router.get("/friends/:id", async (req, res) => {
+
   try {
     const listData = await List.findAll({
       where: {
@@ -33,8 +23,15 @@ router.get("/friends", async (req, res) => {
       },
     });
     const lists = listData.map((list) => list.get({ plain: true }));
-    res.render("friends", { lists });
-  } catch (err) {}
+
+
+
+    const allUserData = await User.findAll();
+    const users = allUserData.map((user) => user.get({ plain: true }));
+    console.log(users);
+
+    res.render("friends", { lists, users });
+  } catch (err) { }
 });
 
 router.get("/profile", async (req, res) => {
@@ -49,15 +46,12 @@ router.get("/profile", async (req, res) => {
     console.log(users);
 
     const lists = listdata.map((list) => list.get({ plain: true }));
-    // res.status(200).json(listdata)
+
     res.render("profile", {
       lists,
       users,
     });
-    // res.render('profile', {
 
-    // });
-    // res.status(200).json(listdata)
   } catch (err) {
     res.status(400).json(err);
   }
@@ -65,10 +59,7 @@ router.get("/profile", async (req, res) => {
 
 // Login route
 router.get("/login", (req, res) => {
-  // if (req.session.loggedIn) {
   res.redirect("/");
-  // return;
-  // }
   res.render("login");
 });
 
@@ -77,55 +68,17 @@ router.get("/:id", async (req, res) => {
     const userData = await User.findByPk(req.params.id, {
       include: [{ model: List }],
     });
-    // const users = userData.map((user) =>
-    //     user.get({ plain: true })
-    // );
+
     const user = userData.get({ plain: true });
     res.status(200).json(userData);
     res.render("homepage", {
       user,
-      // loggedIn: req.session.loggedIn,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// router.get('/', async (req, res) => {
-//     try {
 
-//         const listdata = await List.findAll({
-
-//         })
-//         const lists = listdata.map((user) =>
-//             user.get({ plain: true })
-//         );
-//         // res.status(200).json(listdata)
-//         res.render('homepage', {
-//             lists
-//             // loggedIn: req.session.loggedIn,
-//         });
-//         // res.status(200).json(listdata)
-//     }
-//     catch (err) {
-//         res.status(400).json(err)
-//     }
-
-// });
-
-// router.get('/:id', async (req, res) => {
-//     try {
-//         const listdata = await List.findByPk(req.params.id)
-//         const list = listdata.get({ plain: true });
-//         // res.status(200).json(list)
-//         res.render('homepage', {
-//             list
-//             // loggedIn: req.session.loggedIn,
-//         });
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-
-// });
 
 module.exports = router;
