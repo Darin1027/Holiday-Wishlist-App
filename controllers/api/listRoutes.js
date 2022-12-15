027e5e5cb0e0c6d7942d0f67088a38f05c0fb3e5
DareEnd <114109917+Darin1027@users.noreply.github.com>
2022-12-15 12:25:46 -0700

    updated handlebars to show particle.js snow on profile and friends page

--- controllers/api/listRoutes.js
+++ controllers/api/listRoutes.js
@@ -1,23 +1,7 @@
-<<<<<<< HEAD
 const router = require("express").Router();
 const { User, List } = require("../../models");
-=======
-const router = require('express').Router();
-const { User, List } = require('../../models');
-
 
 // create new item
-router.post('/', async (req, res) => {
-    const body = req.body
-    try {
-        const listdata = await List.create({ ...body, user_id: req.session.user_id })
-
-        res.status(200).json(listdata);
-    } catch (err) {
-        res.status(400).json(err);
-    }
->>>>>>> main
-
 router.post("/", async (req, res) => {
   const body = req.body;
   try {
@@ -25,55 +9,37 @@ router.post("/", async (req, res) => {
       ...body,
       user_id: req.session.user_id,
     });
+
     res.status(200).json(listdata);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-<<<<<<< HEAD
+// update item
 router.put("/:id", async (req, res) => {
   console.log(req.body);
   try {
-    const [affectedRows] = await List.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
+    const [affectedRows] = await List.update(
+      req.body,
+
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
     if (affectedRows > 0) {
       res.status(200).end();
     } else {
       res.status(404).end();
-=======
-// update item
-router.put('/:id', async (req, res) => {
-    console.log(req.body)
-    try {
-        const [affectedRows] = await List.update(req.body,
-
-            {
-                where: {
-                    id: req.params.id
-                }
-            }
-
-        )
-        if (affectedRows > 0) {
-            res.status(200).end();
-        }
-        else {
-            res.status(404).end()
-        }
-    } catch (err) {
-        res.status(400).json(err);
->>>>>>> main
     }
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-<<<<<<< HEAD
+// delete item
 router.delete("/:id", async (req, res) => {
   try {
     const listdata = await List.destroy({
@@ -85,20 +51,6 @@ router.delete("/:id", async (req, res) => {
   } catch (err) {
     res.status(400).json(err);
   }
-=======
-// delete item
-router.delete('/:id', async (req, res) => {
-    try {
-        const listdata = await List.destroy({
-            where: {
-                id: req.params.id
-            }
-        })
-        res.status(200).json(listdata);
-    } catch (err) {
-        res.status(400).json(err);
-    }
->>>>>>> main
 });
 
 module.exports = router;
